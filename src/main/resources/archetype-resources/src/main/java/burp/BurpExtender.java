eb796d08c11c531fb7acfefac144ba10e030c780
i.elkin <i.elkin@qiwi.ru>
2015-09-04 11:43:53 +0300

    archetype resources

--- src/main/resources/archetype-resources/src/main/java/burp/BurpExtender.java
+++ src/main/resources/archetype-resources/src/main/java/burp/BurpExtender.java
@@ -5,6 +5,8 @@ public class BurpExtender implements IBurpExtender {
     @Override
     public void registerExtenderCallbacks(IBurpExtenderCallbacks callbacks) {
 
+        callbacks.setExtensionName("My Burp Suite Plugin");
+
     }
 
 }
